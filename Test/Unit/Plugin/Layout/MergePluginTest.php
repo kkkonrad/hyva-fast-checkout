d0e30efd5cac2f8737470439534463259abaf892
Administrator <admin@hosting-magento.pl>
2026-08-02 19:27:17 +0000

    Apply Hyvä layout container fixes on all pages
    - Drop Fastcheckout-only action guard from MergePlugin
    - Convert head.additional/before.body.end containers site-wide
    - Update unit tests for unconditional conversion

--- Test/Unit/Plugin/Layout/MergePluginTest.php
+++ Test/Unit/Plugin/Layout/MergePluginTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Kkkonrad\Fastcheckout\Test\Unit\Plugin\Layout;
 
 use Kkkonrad\Fastcheckout\Plugin\Layout\MergePlugin;
-use Magento\Framework\App\Request\Http as HttpRequest;
 use Magento\Framework\View\Model\Layout\Merge;
 use PHPUnit\Framework\TestCase;
 use SimpleXMLElement;
@@ -14,31 +13,27 @@ class MergePluginTest extends TestCase
 {
     public function testConvertsPayuCompatibilityNodesOnFastcheckout(): void
     {
-        $request = $this->createMock(HttpRequest::class);
-        $request->method('getFullActionName')->willReturn('fastcheckout_index_index');
         $xml = new SimpleXMLElement(
             '<layout><referenceContainer name="head.additional"><block name="payu"/></referenceContainer>' .
             '<referenceContainer name="before.body.end"><block name="payu.script"/></referenceContainer></layout>'
         );
 
-        (new MergePlugin($request))->afterAsSimplexml($this->createMock(Merge::class), $xml);
+        (new MergePlugin())->afterAsSimplexml($this->createMock(Merge::class), $xml);
 
         self::assertCount(2, $xml->xpath('//referenceBlock'));
         self::assertSame('payu', (string)$xml->referenceBlock[0]->block['name']);
         self::assertSame('payu.script', (string)$xml->referenceBlock[1]->block['name']);
     }
 
-    public function testLeavesOtherFrontendPagesUntouched(): void
+    public function testConvertsCompatibilityNodesOnOtherFrontendPages(): void
     {
-        $request = $this->createMock(HttpRequest::class);
-        $request->method('getFullActionName')->willReturn('catalog_product_view');
         $xml = new SimpleXMLElement(
             '<layout><referenceContainer name="before.body.end"><block name="product.script"/></referenceContainer></layout>'
         );
 
-        (new MergePlugin($request))->afterAsSimplexml($this->createMock(Merge::class), $xml);
+        (new MergePlugin())->afterAsSimplexml($this->createMock(Merge::class), $xml);
 
-        self::assertCount(1, $xml->xpath('//referenceContainer'));
-        self::assertCount(0, $xml->xpath('//referenceBlock'));
+        self::assertCount(0, $xml->xpath('//referenceContainer'));
+        self::assertCount(1, $xml->xpath('//referenceBlock'));
     }
 }
